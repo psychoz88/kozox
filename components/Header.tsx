0f2823504406b1e74f041a41c203df74146b1b33
volodymyr.zamotailo <volodymyr.zamotailo@ionidea.com>
2022-07-16 14:10:35 +0300

    npm start

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/header.module.scss';
+
+const Header = () => {
+  return (
+    <>
+      <h1 className={styles.header}>Kozox</h1>
+    </>
+  );
+};
+
+export default Header;
